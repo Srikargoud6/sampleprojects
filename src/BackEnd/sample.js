b13833be187c75c63413b463a45614b98e77f7a6
Srikargoud6 <srikargoud611@gmail.com>
2024-08-03 16:49:40 +0530

    express routing

--- src/BackEnd/sample.js
+++ src/BackEnd/sample.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("We got a new request");
-  res.send("This is a response!");
+// app.use((req, res) => {
+//   console.log("We got a new request");
+//   res.send("This is a response!");
+// });
+
+app.get("/", (req, res) => {
+  res.send("This is a home page");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.send(`<h1>This is ${subreddit} subreddit</h1>`);
+  console.log(req.params);
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is an about page");
+});
+
+app.get("*", (req, res) => {
+  res.send("Unknown path");
 });
 
 app.listen(4000, () => {
