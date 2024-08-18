fa086ca01f125fa9346247ad5727edfd59b7cff5
Srikargoud6 <srikargoud611@gmail.com>
2024-08-18 11:31:28 +0530

    creating and updating products

--- src/BackEnd/Mongoose Express/index.js
+++ src/BackEnd/Mongoose Express/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 
 const mongoose = require("mongoose");
 
@@ -12,16 +13,41 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
 
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/details", { product });
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  // Product.insertMany(req.body).then((data) => console.log(data));
+  res.redirect(`/products/${newProduct.id}`);
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndUpdate(id, req.body);
+  res.redirect(`/products/${id}`);
 });
 
 app.listen(4000, () => {
