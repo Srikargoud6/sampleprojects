162d1d1592a1f7603a5610167af6e2e1e84030a3
Srikargoud6 <srikargoud611@gmail.com>
2025-08-04 11:33:38 +0530

    datatypes, functions in typescript

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
 let age = 25;
-console.log(`My age is ${age}`);
+let COURSE_NAME = "TypeScript Basics";
+let numbers = [1, 2, 3, 4, 5];
+let mixedArray = [1, "two", 3, "four", 5];
+//tuple: fixed size and type array
+let tuple = [1, "one"];
+tuple.push("extra"); // This will cause a type error because the tuple is defined to only accept a number and a string
+//enum: a way to define a set of named constants
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+function square(num) {
+    return num * num;
+}
+function product(num1, num2 = 5) {
+    return num1 * num2;
+}
+let employee = { id: 321, name: "john" };
+// console.log(mySize); // Output: 2
+console.log(employee);
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,2 +1,35 @@
 let age: number = 25;
-console.log(`My age is ${age}`);
+let COURSE_NAME: string = "TypeScript Basics";
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let mixedArray: (number | string)[] = [1, "two", 3, "four", 5];
+
+//tuple: fixed size and type array
+let tuple: [number, string] = [1, "one"];
+tuple.push("extra"); // This will cause a type error because the tuple is defined to only accept a number and a string
+
+//enum: a way to define a set of named constants
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+
+function square(num: number): number {
+  return num * num;
+}
+
+function product(num1: number, num2 = 5): number {
+  return num1 * num2;
+}
+
+let employee: {
+  readonly id: number;
+  name: string;
+  address?: string;
+} = { id: 321, name: "john" };
+
+// console.log(mySize); // Output: 2
+
+console.log(employee);
