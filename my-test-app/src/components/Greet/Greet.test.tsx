bfa548764e97b6621c918fb95d5e088c66338a59
Srikargoud6 <srikargoud611@gmail.com>
2025-08-05 11:39:11 +0530

    grouped tests

--- my-test-app/src/components/Greet/Greet.test.tsx
+++ my-test-app/src/components/Greet/Greet.test.tsx
@@ -1,14 +1,16 @@
 import Greet from "./Greet";
 import { render, screen } from "@testing-library/react";
 
-test("renders Greet message", () => {
-  render(<Greet />);
-  const greetMessage = screen.getByText(/Hello/i);
-  expect(greetMessage).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders Greet message", () => {
+    render(<Greet />);
+    const greetMessage = screen.getByText(/Hello/i);
+    expect(greetMessage).toBeInTheDocument();
+  });
 
-test("renders greet message followed by name", () => {
-  render(<Greet name="Srikar" />);
-  const greetMessage = screen.getByText(/Hello Srikar/i);
-  expect(greetMessage).toBeInTheDocument();
+  test("renders greet message followed by name", () => {
+    render(<Greet name="Srikar" />);
+    const greetMessage = screen.getByText(/Hello Srikar/i);
+    expect(greetMessage).toBeInTheDocument();
+  });
 });
