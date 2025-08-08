6c615e156989c178236ba89cc9de151d450aa617
Srikargoud6 <srikargoud611@gmail.com>
2025-08-08 13:14:11 +0530

    other misc. query methods

--- my-test-app/src/components/Application/Application.test.tsx
+++ my-test-app/src/components/Application/Application.test.tsx
@@ -3,11 +3,17 @@ import Application from "./Application";
 import { screen } from "@testing-library/dom";
 import { render } from "@testing-library/react";
 
+// https://testing-library.com/docs/
+
 describe("Application ", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox", { name: "Name" });
+
+    const nameElement = screen.getByLabelText("Name");
     expect(nameElement).toBeInTheDocument();
+
+    const nameElement2 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement2).toBeInTheDocument();
 
     const bioElement = screen.getByRole("textbox", { name: "Bio" });
     expect(bioElement).toBeInTheDocument();
@@ -17,6 +23,12 @@ describe("Application ", () => {
 
     const subHeadingElement = screen.getByRole("heading", { level: 2 });
     expect(subHeadingElement).toBeInTheDocument();
+
+    const imgElement = screen.getByAltText("a person with a laptop");
+    expect(imgElement).toBeInTheDocument();
+
+    const testElement = screen.getByTestId("custom-element");
+    expect(testElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
