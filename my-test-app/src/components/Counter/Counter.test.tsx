0c0eb057fc49e2c57478fd6f3a3498e4ea79b286
Srikargoud6 <srikargoud611@gmail.com>
2025-08-15 10:35:19 +0530

    added test to check keyboard interactions

--- my-test-app/src/components/Counter/Counter.test.tsx
+++ my-test-app/src/components/Counter/Counter.test.tsx
@@ -31,4 +31,35 @@ describe("Counter", () => {
     const countElement = screen.getByRole("heading", { level: 2 });
     expect(countElement).toHaveTextContent("1");
   });
+
+  test("render amount of 10 after clicking set button", async () => {
+    user.setup();
+    render(<Counter />);
+
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+
+    const setButton = screen.getByRole("button", { name: "set" });
+    await user.click(setButton);
+
+    const countElement = screen.getByRole("heading", { level: 2 });
+    expect(countElement).toHaveTextContent("1");
+  });
+
+  // test("handlers are called", async () => {
+  //   user.setup();
+  //   const incrementHandler = jest.fn();
+  //   const decrementHandler = jest.fn();
+  //   render(<Counter />);
+
+  //   const incrementButton = screen.getByRole("button", { name: "+" });
+  //   const decrementButton = screen.getByRole("button", { name: "-" });
+
+  //   await user.click(incrementButton);
+  //   await user.click(decrementButton);
+
+  //   expect(incrementHandler).toHaveBeenCalledTimes(1);
+  //   expect(decrementHandler).toHaveBeenCalledTimes(1);
+  // });
 });
--- my-test-app/src/components/Counter/Counter.tsx
+++ my-test-app/src/components/Counter/Counter.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
+  const [amount, setAmount] = useState(0);
   const addCount = () => {
     setCount((prev) => {
       return prev + 1;
@@ -16,12 +16,21 @@ const Counter = () => {
       return prev - 1;
     });
   };
+
   return (
     <div>
       <h1>Counter</h1>
       <button onClick={minusCount}>-</button>
       <h2>{count}</h2>
       <button onClick={addCount}>+</button>
+      <input
+        type="number"
+        name="amount"
+        id=""
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />
+      <button onClick={() => setCount(amount)}>set</button>
     </div>
   );
 };
